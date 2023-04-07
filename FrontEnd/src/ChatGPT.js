f246e970c935b9364560da841705e98a4e51e3ab
Robert <robertoana47@gmail.com>
2023-04-07 12:38:13 +0300

    design&api openai

--- FrontEnd/src/ChatGPT.js
+++ FrontEnd/src/ChatGPT.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import './ChatGPT.css'
+import { useState, useEffect } from 'react'
 export function ChatGPT(){
-function handleSubmit(){
-    async function handleSubmit(e){
-      e.preventDefault();
-    }
-  }
 
+  const [value, setValue] = useState('')
+
+  const [message, setMessage] = useState('')
+
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    try {
+      const response = await fetch('http://localhost:8001/completions', options);
+      const data = await response.json();
+      let message = '';
+      if (data && data.message) {
+        message = data.message;
+      }
+      setMessage(message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  console.log(message)
   return (
     <div className="App">
       <aside className="sidemenu">
@@ -14,39 +38,30 @@ function handleSubmit(){
           <span>+</span>
           New Chat
         </div>
+        <nav>
+          <p>Made by Dribble!</p>
+        </nav>
       </aside>
-      <section className="chatbox">
-        <div className="chat-log">
-          <div className="chat-message">
-              <div className="chat-message-centre">
-               <div className="avatar">
-               
-               </div>
-               <div className="message">
-                 Hello World
-               </div>
-              </div>
+
+      <section className="main">
+        <h1>Dribble</h1>
+
+        <ul className="feed">
+        <li>{message}</li>
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>
+              -
             </div>
-            <div className="chat-message chatgpt">
-              <div className="chat-message-centre">
-               <div className="avatar chatgpt">
-               
-               </div>
-               <div className="message">
-                 I am an AI
-               </div>
-              </div>
-            </div>
-        </div>
-          <div className="chat-input">
-            <form onSubmit={handleSubmit}>
-            <input rows="1"/*te lasa sa scrii doar pe un rand*/
-              className="chat-input-textarea" 
-              placeholder="Type your message here">
-            </input>
-            </form>
+            <p className="info">
+              CHAT GPT
+            </p>
           </div>
+        </div>
       </section>
+
     </div>
 
   )
